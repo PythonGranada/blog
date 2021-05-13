25eb776a2290a58a6a91bd781cd3a57210c4dc03
josemi <josemiguellopez@glampinghub.com>
2021-05-13 09:51:03 +0200

    First commit :tada:

---
+++ pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Python Granada Org'
+SITENAME = 'Python Granada'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = "themes/buruma"
+TIMEZONE = 'Europe/Madrid'
+DEFAULT_LANG = 'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["i18n_subsites", "assets"]
+
+JINJA_ENVIRONMENT = {
+    "extensions": ["jinja2.ext.i18n"],
+}
+
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+SITEURL="https://pythongranada.github.io/blog"
+
+# Theme config
+MENUITEMS_NAVBAR = (("Asociación", f"{SITEURL}/pages/about.html"),)
+NAVBAR_STYLE = "is-primary"
+THEME_LOGO = f"{SITEURL}/theme/images/logo_grande.svg"
+FOOTER= "Made with ❤️ with Python from Granada. Under construction 🚧"
